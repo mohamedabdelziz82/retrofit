d3cb0418e69fc5f892b6989d16f6644f53626ce3
Mohamed Abdelaziz <mhmadzizo82@gmail.com>
2019-11-06 20:21:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mohamedabdelazizhamad/retrofit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mohamedabdelazizhamad.retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
